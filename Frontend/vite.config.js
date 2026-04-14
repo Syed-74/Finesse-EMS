522aea8f53562a4be98ed799145d4078679fa75d
Syed-74 <syednusrath380@gmail.com>
2026-04-14 14:22:10 +0530

    remove server code from view.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const VITE_API_BASE_URL = process.env.VITE_API_BASE_URL;
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port:3000,
-    // host: VITE_API_BASE_URL,
-  },
 })
